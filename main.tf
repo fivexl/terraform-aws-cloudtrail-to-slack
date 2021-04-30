49e34afea12b34478d67cbbaab148c626c27304a
Andrey Devyatkin <andrey.devyatkin@fivexl.io>
2021-04-30 14:08:58 +0200

    fix lambda module version
    Signed-off-by: Andrey Devyatkin <andrey.devyatkin@fivexl.io>

--- main.tf
+++ main.tf
@@ -1,5 +1,6 @@
 module "lambda" {
-  source = "terraform-aws-modules/lambda/aws"
+  source  = "terraform-aws-modules/lambda/aws"
+  version = "1.48.0"
 
   function_name = var.function_name
   description   = "Send CloudTrail Events to Slack"
