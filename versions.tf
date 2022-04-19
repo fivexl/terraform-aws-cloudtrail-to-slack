6b23ad38a52dfc204a0473b5b2d9fb5a78b8265a
Vladimir Samoylov <51059484+cageyv@users.noreply.github.com>
2022-04-19 22:11:15 +0700

    feat: Updated AWS provider to version 4.8+ (#22)
    * Update versions.tf
    
    * update lambda to 3.1.1

--- versions.tf
+++ versions.tf
@@ -1,7 +1,22 @@
 terraform {
-  required_version = ">= 0.12.31"
+  required_version = ">= 0.13.1"
 
   required_providers {
-    aws = "~>3.0, >= 3.43"
+    aws = {
+      source  = "hashicorp/aws"
+      version = ">= 4.8"
+    }
+    external = {
+      source  = "hashicorp/external"
+      version = ">= 1.0"
+    }
+    local = {
+      source  = "hashicorp/local"
+      version = ">= 1.0"
+    }
+    null = {
+      source  = "hashicorp/null"
+      version = ">= 2.0"
+    }
   }
 }
