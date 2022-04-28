4e24acb5d636b7a345c5b0452f71d296b581af35
Andrey Devyatkin <andrey.devyatkin@fivexl.io>
2022-04-28 16:57:11 +0200

    update lambda module 3.1.1 -> 3.2.0
    Signed-off-by: Andrey Devyatkin <andrey.devyatkin@fivexl.io>

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
 module "lambda" {
   source  = "terraform-aws-modules/lambda/aws"
-  version = "3.1.1"
+  version = "3.2.0"
 
   function_name = var.function_name
   description   = "Send CloudTrail Events to Slack"
