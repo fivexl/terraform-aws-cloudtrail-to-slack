ade970314beab3487107aeaef0f968486f450de9
jcua-invision <77296048+jcua-invision@users.noreply.github.com>
2022-05-20 14:47:56 -0700

    Update main.tf
    Add optional filter_prefix variable

--- main.tf
+++ main.tf
@@ -89,7 +89,7 @@ resource "aws_s3_bucket_notification" "bucket_notification" {
   lambda_function {
     lambda_function_arn = module.lambda.lambda_function_arn
     events              = ["s3:ObjectCreated:*", "s3:ObjectRemoved:*"]
-    filter_prefix       = "AWSLogs/"
+    filter_prefix       = var.filter_prefix
     filter_suffix       = ".json.gz"
   }
 
