3b20a8200025a56f19b4656e945f9be2c4b285fd
Saef Taher <staher@stroeer.de>
2022-08-17 14:53:55 +0200

    Fix AttachUserPolicy rule

--- src/rules.py
+++ src/rules.py
@@ -31,4 +31,4 @@ default_rules.append('event.get("errorCode", "") == "AccessDenied" ' +
 # Notify about all non-read actions done by root
 default_rules.append('event.get("userIdentity.type", "") == "Root" ' +
                      'and not event["eventName"].startswith(("Get", "List", "Describe", "Head"))')
-default_rules.append('event["eventName"] == "AttachUserPolicy" and event.get("requestParameters.policyArn", "") == "AdministratorAccess"')
+default_rules.append('event["eventName"] == "AttachUserPolicy" and "AdministratorAccess" in event.get("requestParameters.policyArn", "")')
