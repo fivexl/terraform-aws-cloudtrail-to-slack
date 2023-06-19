3fb13477fd2a593cf923d9230c86a920ecf8a6c7
Anton <125114167+EreminAnton@users.noreply.github.com>
2023-06-19 11:26:47 +0500

    undo upd provider

--- versions.tf
+++ versions.tf
@@ -4,7 +4,7 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = ">= 5.0.1"
+      version = ">= 4.8"
     }
     external = {
       source  = "hashicorp/external"
